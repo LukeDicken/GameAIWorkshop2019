c00cd00837336da17700949ba08d654fb6ae1b6c
Luke Dicken <ldicken@zynga.com>
2019-05-24 21:44:13 -0700

    simple Falcon API, stubs for CustomEvent and SessionStart, Status route hooked up

--- IngestionEngine/Launcher.py
+++ IngestionEngine/Launcher.py
@@ -1,5 +1,17 @@
-import falcon
+import falcon, sqlite3
 
+class Status:
 
+    def on_get(self, req, resp):
+        resp.body = "HELLO WORLD"
+
+class CustomEvent:
+
+    pass
+
+class SessionStart:
+
+    pass
 
 api = application = falcon.API()
+api.add_route('/status', Status())
