3e33d120f6a0ebcfa71cd4ea67380f2df715fa36
Luke Dicken <ldicken@zynga.com>
2019-05-26 17:44:46 -0700

    PlayerID system

--- WorkshopDemo/Assets/Scripts/SceneManager.cs
+++ WorkshopDemo/Assets/Scripts/SceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class SceneManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class SceneManager : MonoBehaviour
     void Start()
     {
         Debug.Log("stuff");
+        manage_playerID();
         AnalyticsManager.logSessionStart();
         if (ChangeConfig)
         {
@@ -17,6 +19,7 @@ public class SceneManager : MonoBehaviour
             int i = Random.Range(1, 3);
             switch (i)
             {
+                // TODO - flip this out for a query
                 case 1:
                     sun_config1();
                     break;
@@ -53,5 +56,32 @@ public class SceneManager : MonoBehaviour
         sun.transform.rotation = Quaternion.Euler(rotation);
         Debug.Log("Config 2");
 
+    }
+
+    void manage_playerID()
+    {
+        if(PlayerPrefs.HasKey("PlayerID"))
+        {
+            // already set - we're good
+        }
+        else
+        {
+            /*
+             * In an ideal world, Identity would be handled outside server-side
+             * Identity is genuinely one of the hardest things in Analytics,
+             * since there are so many freaking edge cases around:
+             *      Anonymous device usage
+             *      Social network linkage
+             *      Shared devices
+             * Also be aware that tracking an identity can causes issues with
+             * GDPR and similar systems.
+             * 
+             * For this exercise we are not tracking any identifiable info, so
+             * we're safe
+             */
+            int rand = Random.Range(0, 1000);
+            string hash = Hash128.Compute(SystemInfo.deviceUniqueIdentifier + rand.ToString()).ToString();
+            PlayerPrefs.SetString("PlayerID", hash);
+        }
     }
 }
