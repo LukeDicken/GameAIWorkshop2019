8b252972208f6aeebe23ce392aa7b3b97c1a57cf
Luke Dicken <ldicken@zynga.com>
2019-05-26 17:36:19 -0700

    Hacking around with SQLite while offline

--- IngestionEngine/Launcher.py
+++ IngestionEngine/Launcher.py
@@ -1,4 +1,4 @@
-import falcon, sqlite3
+import sqlite3, pandas as pd
 
 class Status:
 
@@ -13,5 +13,23 @@ class SessionStart:
 
     pass
 
-api = application = falcon.API()
-api.add_route('/status', Status())
+# api = application = falcon.API()
+# api.add_route('/status', Status())
+
+def sql_play():
+    sql = sqlite3.Connection(database='data/hello.sql')
+    # try:
+    #     sql.execute("CREATE TABLE test1 (stat_date timestamp, player_id int, metric varchar(50), payload varchar(100))")
+    # except sqlite3.OperationalError:
+    #     print("Table already exists")
+    #sql.execute("INSERT INTO test1 (player_id, metric, payload) VALUES (5, 'hello', 'world')")
+    #sql.commit()
+    thing = sql.execute("SELECT * FROM test1")
+    df = pd.DataFrame(thing)
+    print(df)
+
+
+
+
+if __name__ == "__main__":
+    sql_play()
