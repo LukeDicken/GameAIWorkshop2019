05c4f033eb938b7be791b8a080ad18fba5ee5393
Luke Dicken <ldicken@zynga.com>
2019-05-26 17:35:53 -0700

    Addeed system to trigger CustomEvents on collision events

---
+++ WorkshopDemo/Assets/Scripts/CollisionAlerter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAlerter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        string colName = other.name; // pull the name of the other collider
+        if(colName == "FPSController")
+        {
+            // the player did this
+            // fire an event
+            AnalyticsManager.logCustomEvent();
+        }
+    }
+}
