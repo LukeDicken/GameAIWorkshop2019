a7770b16911c3508eed8cb88e2b6c5d3642ea934
Luke Dicken <ldicken@zynga.com>
2019-05-29 13:28:38 -0400

    Setup on backend for SessionIDs and delivery of content modulator

--- IngestionEngine/Launcher.py
+++ IngestionEngine/Launcher.py
@@ -30,8 +30,9 @@ class CounterEvent:
         now = dt.datetime.now()
         counterName = req.media.pop('counterName')
         primaryParameter = req.media.pop('primaryParameter')
+        sessionID = req.media.pop("SessionID")
         data = json.dumps(req.media)
-        insertStatement = "INSERT INTO counters (statTime, playerID, counterName, primaryParameter, parameterBlob) VALUES ('" + str(now) + "', '" + str(playerID) + "', '" + str(counterName) + "', '" + str(primaryParameter) + "', '" + str(data) + "')"
+        insertStatement = "INSERT INTO counters (statTime, playerID, counterName, primaryParameter, sessionID, parameterBlob) VALUES ('" + str(now) + "', '" + str(playerID) + "', '" + str(counterName) + "', '" + str(primaryParameter) + "', '" + str(sessionID) + "', '" + str(data) + "')"
         self.db.execute(insertStatement)
         self.db.commit()
         data = {"status":"ok"}
@@ -46,7 +47,8 @@ class SessionStart:
         playerID = req.media['PlayerID']
         now = dt.datetime.now()
         startTime = req.media['startTime']
-        insertStatement = "INSERT INTO sessions (statTime, sessionStartTime, playerID) VALUES ('" + str(now) + "', '" + str(startTime) + "', '" + str(playerID) + "')"
+        sessionID = req.media['SessionID']
+        insertStatement = "INSERT INTO sessions (statTime, sessionStartTime, playerID, sessionID) VALUES ('" + str(now) + "', '" + str(startTime) + "', '" + str(playerID) + "', '" + str(sessionID) + "')"
         print(insertStatement)
         self.db.execute(insertStatement)
         self.db.commit()
@@ -76,17 +78,22 @@ class PlayerType:
     def __init__(self, db):
         self.db = db
 
-    def on_post(self, req, resp):
-        pass
-
+    def on_get(self, req, resp):
+        playerID = req.get_param('PlayerID')
+        getStatement = "SELECT COUNT(1) FROM sessions WHERE playerID = '" + str(playerID) + "'"
+        cursor = self.db.execute("SELECT COUNT(*) FROM sessions")
+        df = pd.DataFrame(cursor)
+        sessionCount = (int)((df.iloc[0][0]).astype(np.int32))
+        evenOdd = sessionCount % 2
+        data = {"status":"ok","configValue":evenOdd}
+        resp.body = json.dumps(data)
 
 def sql_setup():
     sql = sqlite3.Connection(database='data/Datastore.sql')
     try:
         playerSetup = "CREATE TABLE IF NOT EXISTS players (installTime timestamp, playerID varchar(50))"
         sessionSetup = "CREATE TABLE IF NOT EXISTS sessions (statTime timestamp, sessionStartTime timestamp, playerID varchar(50), sessionID varchar(50))"
-        counterSetup = "CREATE TABLE  IF NOT EXISTS counters (statTime timestamp, playerID varchar(50), counterName varchar(50), primaryParameter varchar(50), parameterBlob varchar(250))"
-        modelSetup = "CREATE TABLE IF NOT EXISTS models (trainingTime timestamp, modelName varchar(50), playerID varchar(50), label int)"
+        counterSetup = "CREATE TABLE  IF NOT EXISTS counters (statTime timestamp, sessionID varchar(50), playerID varchar(50), counterName varchar(50), primaryParameter varchar(50), parameterBlob varchar(250))"
         sql.execute(playerSetup)
         sql.execute(sessionSetup)
         sql.execute(counterSetup)
@@ -99,6 +106,10 @@ def sql_setup():
 sql = sql_setup()
 api = application = falcon.API()
 api.add_route('/status', Status(sql))
+
+
+
 api.add_route('/newPlayer', PlayerRegisterEvent(sql))
 api.add_route('/sessionStart', SessionStart(sql))
 api.add_route('/counter', CounterEvent(sql))
+api.add_route('/playerType', PlayerType(sql))
